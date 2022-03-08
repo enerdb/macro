d423c8a8c8a5825cf59d8df73ed03810ff0d1dea
Ener Diniz Beckmann <83406614+enerdb@users.noreply.github.com>
2022-03-08 11:41:42 -0300

    Add files via upload

---
+++ BSS-oo.py
@@ -0,0 +1,146 @@
+﻿
+#####################################
+# Imports
+#####################################
+
+from pynput import mouse
+from pynput import keyboard
+import threading
+
+#####################################
+# Helper functions
+#####################################
+
+def single_click():
+    my_mouse.click(mouse.Button.left)
+
+#####################################
+# Routine Classes
+#####################################
+
+class Routine(threading.Thread):
+
+#""" The Thread class represents an activity that is run in a separate thread of control.
+#There are two ways to specify the activity: by passing a callable object to the constructor,
+#or by overriding the run() method in a subclass.
+#No other methods (except for the constructor) should be overridden in a subclass.
+#In other words, only override the __init__() and run() methods of this class.
+#Parece que estou fazendo override no start()
+#"""
+
+    def __init__(self):
+        super(Routine, self).__init__()  # Chama o construtor de threading.Thread
+        self.running = False
+        
+        
+    def turn_on(self):
+        self.running = True
+        
+    def turn_off(self):
+        self.running = False
+         
+
+#### ClickMouse - AutoClick ####
+         
+class ClickMouse(Routine):
+    def __init__(self, delay = 0.001, button = mouse.Button.left):
+        super(ClickMouse, self).__init__()
+        self.delay = delay
+        self.button = button
+        self.running = False
+ 
+    def run(self):
+        while self.running:
+            my_mouse.click(self.button)
+            event.wait(self.delay)
+
+#### Farm ####
+
+class Farm(Routine):
+    def __init__(self):
+        super(Farm, self).__init__()
+        
+    def run(self):
+        orientation = ['w', 'a', 's', 'd']
+        while self.running:
+            ncircles = 3
+            while(ncircles > 0):
+                sleep_time = 0.1 + ncircles*0.3
+                for side in orientation:
+                    if not self.running:
+                        return
+                    my_mouse.click(mouse.Button.left)
+                    my_keyboard.press(side)
+                    event.wait(sleep_time)
+                    my_keyboard.release(side)
+                ncircles -= 1
+ 
+    
+#####################################
+# On press functions
+#####################################    
+
+def stop_all():
+    ac_thread.turn_off()
+    farmer.turn_off()
+    
+def program_quit():
+    stop_all()
+    #listener.stop()
+    return False
+
+
+    
+
+#####################################
+# Main 
+#####################################
+
+if __name__ == "__main__":
+
+    my_mouse = mouse.Controller()
+    my_keyboard = keyboard.Controller()
+    ac_thread = ClickMouse(1, mouse.Button.left)
+    ac_thread.start()
+    farmer = Farm()
+    farmer.start()
+
+
+    event = threading.Event()
+
+
+    with keyboard.GlobalHotKeys({
+            '<ctrl>+f': farmer.turn_on(),
+            '<ctrl>+t': ac_thread.turn_on(),
+            '<ctrl>+g': stop_all(),
+            '<ctrl>+x': program_quit()
+            }) as listener:
+        listener.join()
+
+#######################################################
+# Problemas encontrados:
+# - Não estou conseguindo fazer multithreading e o programa só libera o main quando a função intermediária termina
+# - return False não está funcionando para sair do programa com GlobalHotKeys. Funcionava com Listener
+
+
+
+
+
+
+########################################################
+
+# from pynput import keyboard
+
+# def on_activate():
+    # print('Global hotkey activated!')
+
+# def for_canonical(f):
+    # return lambda k: f(l.canonical(k))
+
+# hotkey = keyboard.HotKey(
+    # keyboard.HotKey.parse('<ctrl>+<alt>+h'),
+    # on_activate)
+# with keyboard.Listener(
+        # on_press=for_canonical(hotkey.press),
+        # on_release=for_canonical(hotkey.release)) as l:
+    # l.join()
